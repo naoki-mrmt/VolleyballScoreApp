95f6b9325d3d871d8baed37d9920e7fd1bc3f157
naoki-mrnk <20cm0135@jec.ac.jp>
2022-12-30 00:10:49 +0900

    add project

---
+++ VolleyballScoreApp/VolleyballScoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolleyballScoreAppApp.swift
+//  VolleyballScoreApp
+//
+//  Created by Naoki Muramoto on 2022/12/30.
+//
+
+import SwiftUI
+
+@main
+struct VolleyballScoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
