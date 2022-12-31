d59112d5ddf22efc241dfa42b1cffb1296f6ef8c
naoki-mrmt <20cm0135@jec.ac.jp>
2022-12-31 17:56:57 +0900

    add launch animation

--- VolleyballScoreApp/Screen/Launch/LaunchView.swift
+++ VolleyballScoreApp/Screen/Launch/LaunchView.swift
@@ -8,8 +8,70 @@
 import SwiftUI
 
 struct LaunchView: View {
+    // MARK: - Property Wrappers
+    @State private var isLoading = true
+    @State private var animationState: AnimationState = .normal
+
+    // MARK: - Properties
+    private let logoSize = UIScreen.main.bounds.height * 0.4
+
+    // MARK: - Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isLoading {
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .scaleEffect(calculateLogoSize())
+                .onAppear { animationTask() }
+        } else {
+            HomeView()
+        }
+    }
+}
+
+// MARK: - enum
+extension LaunchView {
+    private enum AnimationState {
+        case normal
+        case compress
+        case expand
+    }
+}
+
+// MARK: - Method
+extension LaunchView {
+    private func calculateLogoSize() -> Double {
+        switch animationState {
+        case .normal:
+            return 0.2
+        case .compress:
+            return 0.18
+        case .expand:
+            return 10.0
+        }
+    }
+
+    private func animationTask() {
+        Task {
+            try await Task.sleep(for: .seconds(0.75))
+            withAnimation(.spring()) {
+                animationState = .compress
+                Task {
+                    try await Task.sleep(for: .seconds(0.5))
+                    withAnimation(.spring()) {
+                        animationState = .expand
+                        withAnimation(.interpolatingSpring(
+                            mass: 1.0,
+                            stiffness: 100.0,
+                            damping: 10.0,
+                            initialVelocity: 0
+                        )) {
+                            isLoading = false
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
