086c3474c2ba8b39c5fd8d3e05aa55cda8fec0d8
naoki-mrmt <20cm0135@jec.ac.jp>
2022-12-31 17:42:20 +0900

    add LaunchView

---
+++ VolleyballScoreApp/Screen/Launch/LaunchView.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchView.swift
+//  VolleyballScoreApp
+//
+//  Created by Naoki Muramoto on 2022/12/31.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView()
+    }
+}
