7cc81a8aff20655f77f4e2656851056b6a41ce0e
naoki-mrmt <20cm0135@jec.ac.jp>
2023-01-02 16:31:29 +0900

    add MatchResultView

---
+++ VolleyballScoreApp/Screen/MatchResult/MatchResultView.swift
@@ -0,0 +1,20 @@
+//
+//  MatchResultView.swift
+//  VolleyballScoreApp
+//
+//  Created by Naoki Muramoto on 2023/01/02.
+//
+
+import SwiftUI
+
+struct MatchResultView: View {
+    var body: some View {
+        Text("MatchResultView")
+    }
+}
+
+struct MatchResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        MatchResultView()
+    }
+}
