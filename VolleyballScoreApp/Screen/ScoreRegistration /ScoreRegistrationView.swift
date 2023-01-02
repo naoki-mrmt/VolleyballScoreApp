045d90a5a4ec7079915c4944c1c22146a1f27257
naoki-mrmt <20cm0135@jec.ac.jp>
2023-01-02 16:29:38 +0900

    add ScoreRegistrationView

---
+++ VolleyballScoreApp/Screen/ScoreRegistration /ScoreRegistrationView.swift
@@ -0,0 +1,20 @@
+//
+//  ScoreRegistrationView.swift
+//  VolleyballScoreApp
+//
+//  Created by Naoki Muramoto on 2023/01/02.
+//
+
+import SwiftUI
+
+struct ScoreRegistrationView: View {
+    var body: some View {
+        Text("ScoreRegistrationView")
+    }
+}
+
+struct ScoreRegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScoreRegistrationView()
+    }
+}
