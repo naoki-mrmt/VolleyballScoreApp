d08ef39f73f0b9e9eb89f9f2c4f59199cd79d9dc
naoki-mrmt <20cm0135@jec.ac.jp>
2023-01-02 16:24:57 +0900

    add TeamMemberListView

---
+++ VolleyballScoreApp/Screen/TeamMemberList/TeamMemberListView.swift
@@ -0,0 +1,20 @@
+//
+//  TeamMemberListView.swift
+//  VolleyballScoreApp
+//
+//  Created by Naoki Muramoto on 2023/01/02.
+//
+
+import SwiftUI
+
+struct TeamMemberListView: View {
+    var body: some View {
+        Text("TeamMemberListView")
+    }
+}
+
+struct TeamMemberListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TeamMemberListView()
+    }
+}
