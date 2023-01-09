48cb6d06b773adfd3ccce484c3015924f237010d
naoki-mrmt <n.bakupu@gmail.com>
2023-01-09 16:17:29 +0900

    add Color extension

---
+++ VolleyballScoreApp/Extension/ColorExtension.swift
@@ -0,0 +1,14 @@
+//
+//  ColorExtension.swift
+//  VolleyballScoreApp
+//
+//  Created by naoki-mrmt on 2023/01/09.
+//
+
+import SwiftUI
+
+extension Color {
+    static let mainColor = Color(UIColor.mainColor)
+    static let backgroundColor = Color(UIColor.backgroundColor)
+    static let shadowColor = Color(UIColor.shadowColor)
+}
